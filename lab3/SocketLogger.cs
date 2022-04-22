1cdfbdbc1ec7413ac003d4a712d867aa00e4ab05
dakko <dakkon@o2.pl>
2022-04-22 08:08:52 +0200

    costam

--- lab3/SocketLogger.cs
+++ lab3/SocketLogger.cs
@@ -11,7 +11,7 @@ namespace lab3library
         private ClientSocket clientSocket;        
         public SocketLogger(string host, int port)
         {
-            this.clientSocket = new ClientSocket(host, port);
+            clientSocket = new ClientSocket(host, port);
         }
 
         ~SocketLogger()
@@ -35,34 +35,17 @@ namespace lab3library
             GC.SuppressFinalize(this);
         }
         public void Log(params string[] messages)
-        {
+        {           
+            DateTime currentLog = DateTime.Now;
 
-            
-            DateTime dateTime = DateTime.Now;
-            String currentLog = dateTime.ToString() + " ";
             foreach (var message in messages)
             {
-                currentLog += message + " ";
 
+                string requestText = currentLog + message + " ";
+                byte[] requestBytes = Encoding.UTF8.GetBytes(requestText);
 
-                /* using (var writer = new System.IO.StreamWriter(this.clientSocket.GetStream(), Encoding.UTF8, 1024, true))
-                {
-                     writer.WriteLine(currentLog);
-                }*/
-
-                using (clientSocket)
-                {
-                    byte[] requestBytes = Encoding.UTF8.GetBytes(currentLog);
-                    clientSocket.Send(requestBytes);
-
-                    byte[] responseBuffer = new byte[1024];
-                    int responseSize = clientSocket.Receive(responseBuffer);
-                    String responseText = Encoding.UTF8.GetString(responseBuffer, 0, responseSize);
-
-                    Console.WriteLine(responseText);
-                    Console.WriteLine(dateTime + " " + currentLog + " socket");
-                    clientSocket.Close();
-                }
+                clientSocket.Send(requestBytes);
+  
             }
             
 
